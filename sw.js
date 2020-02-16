6b79c0ff1033f9358ec9d41d1443f7cf07edf05b
HenryDong <943082205@qq.com>
2020-02-16 17:57:44 +0800

    add cache API

--- sw.js
+++ sw.js
@@ -1,14 +1,34 @@
 // selfQ       全局
+const CACHE_NAME = 'cache-v2g'
 self.addEventListener('install', e => {
     console.log('install', e)
     // 参数是Promise在Promise完成后触发，延迟activate
-    e.waitUntil(self.skipWaiting())
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => {
+        cache.addAll(['/', './index.css'])
+    }))
     //TEST
 })
 self.addEventListener('activate', e => {
     console.log('activate', e)
-    e.waitUntil(self.clients.claim())
+    e.waitUntil(caches.keys().then(cacheNames => {
+        return Promise.all(cacheNames.map(cacheName => {
+            if (cacheName !== CACHE_NAME){
+                return caches.delete(cacheName)
+            }
+        }))
+    }))
 })
 self.addEventListener('fetch', e => {
     console.log('fetch', e)
+    e.respondWith(caches.open(CACHE_NAME).then(cache => {
+        return cache.match(e.request).then(response => {
+            if (response) {
+                return response
+            }
+            return fetch(e.request).then(response => {
+                cache.put(e.request, response.clone())
+                return response
+            })
+        })
+    }))
 })
